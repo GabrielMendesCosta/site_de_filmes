f06d9589b3e38421095792c778376cae64938b90
GabrielMendesCosta <gabrielmendescst@gmail.com>
2023-10-10 12:13:59 -0300

    feat: add favorite context/page
    Adicionamos a funcionalidade de adicionar os itens favoritados na pag de
    favoritos, além de estilizar o coração que se encontra como botão para favoritar.

--- cinetag/src/components/Card/index.js
+++ cinetag/src/components/Card/index.js
@@ -1,13 +1,18 @@
+import { useFavoritoContext } from "contextos/Favoritos";
 import styles from "./Card.module.css";
 import iconeFavoritar from "./favoritar.png";
+import inconeDesfavoritar from"./desfavorite.png";
 
 
 function Card({id,titulo,capa}){
+    const{favorito, adicionarFavorito} = useFavoritoContext();
+    const ehFavorito = favorito.some((fav) => fav.id === id);
+    const icone = !ehFavorito ? iconeFavoritar : inconeDesfavoritar;
     return(
         <div className={styles.container}>
             <img src={capa} alt={titulo} className={styles.capa}/>
             <h2>{titulo}</h2>
-            <img src={iconeFavoritar} alt="Favoritar filme" className={styles.favoritar}/>
+            <img src={icone} alt="Favoritar filme" className={styles.favoritar} onClick={()=>{adicionarFavorito({id,titulo,capa})}}/>
         </div>
     )
 }
