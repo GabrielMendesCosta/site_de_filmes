21c9ba1be711aa60b34304ca31e574c9a403b803
GabrielMendesCosta <gabrielmendescst@gmail.com>
2023-10-11 10:35:36 -0300

    feat: add news routes and we clean "routes.js".
    Adicionamos a página de erro, modificamos as páginas do Card, Videos e
    Inicial, além de limparmos o "routes.js" com uma page padrão.

---
+++ cinetag/src/pages/PaginaBase/index.js
@@ -0,0 +1,22 @@
+import Container from "components/Container";
+import Rodape from "components/Rodape";
+import FavoritosProvider from "contextos/Favoritos";
+import { Outlet } from "react-router-dom";
+
+const { default: Cabecalho } = require("components/Cabecalho");
+
+function PaginaBase(){
+    return(
+        <main>
+            <Cabecalho/>
+                <FavoritosProvider>
+                    <Container>
+                        <Outlet/>
+                    </Container>
+                </FavoritosProvider>
+            <Rodape/>
+        </main>
+    )
+}
+
+export default PaginaBase
